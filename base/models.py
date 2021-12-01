649bbe3d75e7862642886b545b0129084b36c91b
MariaEduardaLA <mariaeduardal.8787@gmail.com>
2021-11-30 22:15:51 -0300

    agregando front e back-end

--- base/models.py
+++ base/models.py
@@ -1,13 +1,1 @@
 from django.db import models
-from django.db.models.fields import EmailField
-
-class Usuario(models.Model):
-    nome = models.CharField(max_length=50)
-    email = models.EmailField(max_length=45)
-    senha = models.CharField(max_length=12)
-
-class Administrador(models.Model):
-    nome = models.CharField(max_length=50)
-    email = models.EmailField(max_length=45)
-    senha = models.CharField(max_length=12)
-    
