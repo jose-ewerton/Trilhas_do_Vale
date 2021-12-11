2344f000a8a696fc2dbf7ae6bebd15664d44b3d9
MariaEduardaLA <mariaeduardal.8787@gmail.com>
2021-12-11 18:49:43 -0300

    cadastrando no bd

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PaginaInicial
+from .views import PaginaInicial, salva_cadastro
 
 urlpatterns = [
 path('', PaginaInicial.as_view(), name='index'),
 #path('endereco/',MinhaView.as_view(),nome)
+path('salva_cadastro', salva_cadastro, name='salva_cadastro'),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.generic import TemplateView
+
+from base.models import Usuario
+from paginas.forms import UsuarioForm
 
 class PaginaInicial(TemplateView):
     template_name = 'paginas/index.html'
+
+def salva_cadastro(request):
+    nome= request.POST.get('nome')
+    email= request.POST.get('email')
+    senha= request.POST.get('senha')
+    count = Usuario.objects.filter(nome=nome, email=email).count()
+    if count > 0:
+        messages.error(request, 'Dados já existente, tente outro email ou usuário!')
+        return redirect (reverse('index') + '#modalForm')
+    else:
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect (reverse('index') + '#modalForm')
+
+
+        
