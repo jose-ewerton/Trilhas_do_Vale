51a899f1676335b40af15607dfbfad24379e4efb
MariaEduardaLA <mariaeduardal.8787@gmail.com>
2021-12-13 18:25:42 -0300

    pagina locais inserida

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PaginaInicial, salva_cadastro
-
+from .views import PaginaInicial, salva_cadastro, Locais
+from . import views
 urlpatterns = [
 path('', PaginaInicial.as_view(), name='index'),
 #path('endereco/',MinhaView.as_view(),nome)
 path('salva_cadastro', salva_cadastro, name='salva_cadastro'),
+path('locais/', views.Locais, name='locais'),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views.generic import TemplateView
 
@@ -10,11 +10,14 @@ from paginas.forms import UsuarioForm
 class PaginaInicial(TemplateView):
     template_name = 'paginas/index.html'
 
+def Locais(request):
+    return render(request,'paginas/locais.html') 
+
 def salva_cadastro(request):
     nome= request.POST.get('nome')
     email= request.POST.get('email')
     senha= request.POST.get('senha')
-    count = Usuario.objects.filter(nome=nome, email=email).count()
+    count = Usuario.objects.filter(email=email).count()
     if count > 0:
         messages.error(request, 'Dados já existente, tente outro email ou usuário!')
         return redirect (reverse('index') + '#modalForm')
