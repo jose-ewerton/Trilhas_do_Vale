e29dca53a9157d76075a1aa3966767993e40b4b4
MariaEduardaLA <mariaeduardal.8787@gmail.com>
2021-12-14 21:13:55 -0300

    img do bd no template

--- trilhas/settings.py
+++ trilhas/settings.py
@@ -122,10 +122,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/locais/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
