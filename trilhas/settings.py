876e22367707cc98a4e36210bce649388d8a18a0
MariaEduardaLA <mariaeduardal.8787@gmail.com>
2022-01-09 20:07:27 -0300

    Lib django-on-heroku

--- trilhas/settings.py
+++ trilhas/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +136,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+django_on_heroku.settings(locals())
